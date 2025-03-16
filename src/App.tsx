ff0279cb0992297a628733dbc22d1fa54ca591cc
sohyeon <sohyun9743@naver.com>
2025-03-16 19:10:52 +0900

    header 영역 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-// import {BroswerRouter as Router, Switch, Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+import Home from "./Routes/Home";
+import Header from "./Components/Header";
 
 function App() {
-
   return (
-    <>
-     <p>test
-
-     </p>
-    </>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Tv" element={<Tv />} />
+        <Route path="/Search" element={<Search />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
